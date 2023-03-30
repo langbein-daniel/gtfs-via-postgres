c3decaa0a7250ae2865c61813bbe88bdd464328c
Jannis R <mail@jannisr.de>
2023-03-30 21:15:53 +0200

    ignore stop_timezone when calculating arrivals/departures 🐛✅
    Apparently, stop_timezone was only ever intended for displaying local date+time.
    The current operating practice has been clarified in https://github.com/google/transit/issues/322.

--- test/amtrak-gtfs-2021-10-06.sh
+++ test/amtrak-gtfs-2021-10-06.sh
@@ -23,16 +23,16 @@ and date = '2021-11-26'
 order by t_arrival
 EOF)
 
-# 2021-11-26T15:15:00-06:00
+# 2021-11-26T15:15:00-05:00
 arr1=$(psql --csv -t -c "$query" | head -n 1)
-if [[ "$arr1" != "1637961300" ]]; then
+if [[ "$arr1" != "1637957700" ]]; then
 	echo "invalid 1st t_arrival: $arr1" 1>&2
 	exit 1
 fi
 
-# 2021-11-27T13:45:00-06:00
+# 2021-11-27T13:45:00-05:00
 arrN=$(psql --csv -t -c "$query" | tail -n 1)
-if [[ "$arrN" != "1638042300" ]]; then
+if [[ "$arrN" != "1638038700" ]]; then
 	echo "invalid 2nd t_arrival: $arrN" 1>&2
 	exit 1
 fi
